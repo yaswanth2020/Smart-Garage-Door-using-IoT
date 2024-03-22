972955222694a611e246c25ead7c6384b55dba4d
yaswanth2020 <140269349+yaswanth2020@users.noreply.github.com>
2024-03-22 19:35:34 +0530

    Code.cc

---
+++ Code.cc
@@ -0,0 +1,33 @@
+#include <Stepper .h>
+const int pingPin = 2;
+const int motorPin = 13;
+void setup() {
+  Serial.begin(9600);
+  pinMode(motorPin, OUTPUT);
+}
+void loop() {
+  int duration,cm;
+  pinMode(pingPin,OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+  cm = microsecondsToCentimeters(duration);
+  Serial.print("Dsitance: ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  if(cm<329) {
+    delayWrite(motorPin, HIGH);
+    delay(4000);
+  }
+  else {
+    digitalWrite(motorPin, LOW);
+    delay(1000);
+  }
+  long microsecondsToCentimeters(long microseconds) {
+    return microseconds / 29 / 2;
+  }
